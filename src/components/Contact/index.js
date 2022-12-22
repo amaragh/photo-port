9cde5f97cbba523a141bd08c9769dcfe3fac23a2
amaragh <mercer.alecia@gmail.com>
2022-12-21 20:47:19 -0600

    tests for COntact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -30,7 +30,7 @@ function ContactForm() {
     }
 
     return (<section>
-        <h1>Contact Me</h1>
+        <h1 data-testid="h1tag">Contact Me</h1>
         <form id="contact-form" onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">Name:</label>
@@ -50,7 +50,7 @@ function ContactForm() {
                     <p className="error-text">{errorMessage}</p>
                 </div>
             )}
-            <button type="submit">Submit</button>
+            <button data-testid="button"  type="submit">Submit</button>
         </form>
     </section>)
 }
